dbe91a13f5edc98421949656f5f79c4f7f3a12ff
Ian Davies <idda@amazon.com>
2021-04-19 14:49:06 -0700

    Initial project commit containing base files

---
+++ Amazon.Ion.ObjectMapper.Test/IonSerializerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Amazon.Ion.ObjectMapper.Test
+{
+    [TestClass]
+    public class IonSerializerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Amazon.Ion.ObjectMapper/IonSerializer.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Amazon.Ion.ObjectMapper
+{
+    public class IonSerializer
+    {
+    }
+}
