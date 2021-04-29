a916833baab908fde5326381829ba6e7d7ce469c
Justin Gossett <justing@bitquilltech.com>
2021-04-29 12:23:01 -0700

    Remove unnecessary import

--- Amazon.Ion.ObjectMapper/IonObjectSerializer.cs
+++ Amazon.Ion.ObjectMapper/IonObjectSerializer.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using Amazon.IonDotnet;
 
 namespace Amazon.Ion.ObjectMapper
