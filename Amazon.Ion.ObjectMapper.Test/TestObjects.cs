f34b9de5d9e8e9041c20b767f584c1fff0c65df2
Justin Gossett <justing@bitquilltech.com>
2021-05-28 15:52:24 -0700

    Another style change undo

--- Amazon.Ion.ObjectMapper.Test/TestObjects.cs
+++ Amazon.Ion.ObjectMapper.Test/TestObjects.cs
@@ -53,10 +53,10 @@ namespace Amazon.Ion.ObjectMapper.Test
         public string Brand { get; init; }
 
         [IonField]
-        public string color; 
+        public string color;
 
         [IonField]
-        public bool canOffroad;
+        public bool canOffroad; 
         
         public override string ToString()
         {
