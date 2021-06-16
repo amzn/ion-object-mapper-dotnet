248e6f1d26a6dc6a647c930cd302e7f5aa92e25d
Justin Gossett <justing@bitquilltech.com>
2021-06-16 14:10:37 -0700

    Change Desk's IonPropertyGetter/IonPropertySetter's property name to "desk width"

--- Amazon.Ion.ObjectMapper.Test/TestObjects.cs
+++ Amazon.Ion.ObjectMapper.Test/TestObjects.cs
@@ -379,13 +379,13 @@ namespace Amazon.Ion.ObjectMapper.Test
         public int Depth { get; init; }
         public int Height { get; init; }
 
-        [IonPropertyGetter("length")]
+        [IonPropertyGetter("desk width")]
         public int GetWidth() 
         {
             return this.width;
         }
         
-        [IonPropertySetter("length")]
+        [IonPropertySetter("desk width")]
         public void SetWidth(int width) 
         {
             this.width = width;
