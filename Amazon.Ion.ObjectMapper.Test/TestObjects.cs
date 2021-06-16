680120c45e196a4685c803849e32b3f4caec5a82
Justin Gossett <justing@bitquilltech.com>
2021-06-16 14:08:28 -0700

    Change Desk.width from public to internal

--- Amazon.Ion.ObjectMapper.Test/TestObjects.cs
+++ Amazon.Ion.ObjectMapper.Test/TestObjects.cs
@@ -375,7 +375,7 @@ namespace Amazon.Ion.ObjectMapper.Test
 
     public class Desk
     {
-        public int width;
+        internal int width;
         public int Depth { get; init; }
         public int Height { get; init; }
 
