07cfa5bd2826990408a911276996975124ac39c6
Matthew Ma <mtthman@amazon.com>
2022-01-13 15:50:05 -0800

    Fix error when deserializing Ion blob to Nullable<Guid>

--- Amazon.IonObjectMapper/IonSerializer.cs
+++ Amazon.IonObjectMapper/IonSerializer.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright (c) Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with
@@ -365,7 +365,8 @@ namespace Amazon.IonObjectMapper
             if (ionType == IonType.Blob)
             {
                 if (annotations.Any(s => s.Equals(IonGuidSerializer.ANNOTATION))
-                    || typeof(Guid).IsAssignableFrom(type))
+                    || typeof(Guid).IsAssignableFrom(type)
+                    || typeof(Nullable<Guid>).IsAssignableFrom(type))
                 {
                     var serializer = this.GetPrimitiveSerializer<Guid>(new IonGuidSerializer(this.options));
                     return serializer.Deserialize(reader);
